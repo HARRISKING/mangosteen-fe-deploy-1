6ac7a8f3a046f55c0b87a7aea06e5ca0ec79dcc6
suixing <suixing@uni-ubi.com>
2022-10-29 10:37:16 +0800

    feat: 修改名称

--- src/layouts/TimeTabsLayout.tsx
+++ src/layouts/TimeTabsLayout.tsx
@@ -73,7 +73,7 @@ export const TimeTabsLayout = defineComponent({
     return () => (
       <MainLayout>
         {{
-          title: () => '山竹记账',
+          title: () => '连成记账',
           icon: () => <OverlayIcon />,
           default: () => (
             <>
